6de848999bf20795f0b39d9f832c3946aa9e8004
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-22 16:42:54 +0200

    add-class-domesticappliance

---
+++ DomesticAppliance.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class DomesticAppliance : Product
+{
+    public string brand { get; private set; }
+    public int wattage { get; private set; }
+
+    public DomesticAppliance(string newName, string newDescription, float newPrice, int newVat, string brand, int wattage) :base(newName, newDescription, newPrice, newVat)
+    {
+        this.brand = brand;
+        this.wattage = wattage;
+    }
+}
