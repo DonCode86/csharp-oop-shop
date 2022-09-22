fd1a961c3994d7485937b6d7dcb71fb1b8c64a48
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-22 14:47:33 +0200

    add-class-product

--- Program.cs
+++ Program.cs
@@ -3,14 +3,18 @@ Console.WriteLine("Hello, World!");
 public class Product
 {
     //caratteristiche del Prodotto 
-    private int ProductID;
-    private string ProductName;
-    private string ProductDescription;
-    private float ProductPrice;
-    private int ProductVat;
+    private int ID;
+    private string name;
+    private string description;
+    private float price;
+    private int vat;
 
-    public Product(string newProductName, string newProductDescription, float newProductPrice, int newProductVat)
+    public Product (string name, string description, float price)
     {
-        
+        this.ID = new Random().Next(1, 100);
+        this.name = name;
+        this.description = description;
+        this.price = price;
     }
+   
 }
