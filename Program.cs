f91cd4534e37efae41c3e635a3e6aafd6ccb5694
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-22 14:40:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Product
+{
+    //caratteristiche del Prodotto 
+    private int ProductID;
+    private string ProductName;
+    private string ProductDescription;
+    private float ProductPrice;
+    private int ProductVat;
+
+    public Product(string newProductName, string newProductDescription, float newProductPrice, int newProductVat)
+    {
+        
+    }
+}
